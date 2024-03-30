c5c7472d7e48011cfcd2d03ca7829ae51098999a
selmaaneer <122531288+selmaaneer@users.noreply.github.com>
2024-03-30 20:37:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,13 +15,23 @@ const dbURL = process.env.DB_URL;
 
 app.use(express.json());
 
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: false }));
-
 // Enable CORS for all routes
 app.use(cors({
   origin: ['https://wellness360-frondendside.vercel.app']
 }));
+
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+router.get("/", (req, res) => {
+res.setHeader("Access-Control-Allow-Origin", "*")
+res.setHeader("Access-Control-Allow-Credentials", "true");
+res.setHeader("Access-Control-Max-Age", "1800");
+res.setHeader("Access-Control-Allow-Headers", "content-type");
+res.setHeader( "Access-Control-Allow-Methods", "PUT, POST, GET, DELETE, PATCH, OPTIONS" ); 
+ });
 
 
 mongoose.connect(dbURL, { useNewUrlParser: true, useUnifiedTopology: true })
