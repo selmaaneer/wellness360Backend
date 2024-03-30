6b6c1e7fa95b3609140c32a0b2598d7e0597d7e4
selmaaneer <122531288+selmaaneer@users.noreply.github.com>
2024-03-30 13:28:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,10 +16,12 @@ const dbURL = process.env.DB_URL;
 app.use(express.json());
 
 
-app.use(cors({
-  origin: 'https://wellness360-frondendside.vercel.app',
-  credentials: true 
-}));
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 
 app.use(cookieParser());
