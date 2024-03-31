7e7c04858560bd629f6cecf2f85db1baab992c77
selmaaneer <122531288+selmaaneer@users.noreply.github.com>
2024-03-31 11:52:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -19,9 +19,11 @@ app.use(express.json());
 const corsConfig = {
   origin: "*", 
   credentials: true ,
-  methods: ["GET", "POST", "PUT", "DELETE"]
+  methods: ["GET", "POST", "PUT", "DELETE"],
 
 };
+
+app.options("", cors(corsConfig));
 
 app.use(cors(corsConfig));
 
